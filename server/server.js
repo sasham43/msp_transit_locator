7a42017c536e9ee8a7aff410136b3a8769387458
sasham43 <sashasemail@gmail.com>
2016-04-20 16:46:52 -0500

    Added environment variable

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static('server/public'));
 app.use('/', index);
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   var port = server.address().port;
   console.log('Server listening on port ' + port + '...');
 });
